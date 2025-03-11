c1a30924d0caf1ad577231f4b88ab9347706f14c
Abdelrhman Reda <11410120210225@stud.cu.edu.eg>
2025-03-11 05:12:47 +0200

    launching the application

---
+++ android/app/src/main/kotlin/com/snapnfix/mobile/snapnfix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.snapnfix.mobile.snapnfix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  final _currentLocale = const Locale('en', '');
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
+    return MaterialApp(
+      title: 'SnapNFix',
+      supportedLocales: const [Locale('en', ''), Locale('ar', '')],
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: _currentLocale,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(title: Text(AppLocalizations.of(context)!.hello)),
+            body: Center(child: Text(AppLocalizations.of(context)!.welcome)),
+          );
+        },
+      ),
+    );
+  }
+}
